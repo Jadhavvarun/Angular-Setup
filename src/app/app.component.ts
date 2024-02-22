472cc1a88f57620b707954fc429edfc718c53a91
Jadhavvarun <jadhavvarun67@gmail.com>
2024-02-22 14:50:29 +0530

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
