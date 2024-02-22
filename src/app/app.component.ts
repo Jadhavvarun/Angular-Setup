fa3b30f782dd136cbea05de431bd112055fdbb17
Jadhavvarun <jadhavvarun67@gmail.com>
2024-02-22 14:29:40 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
