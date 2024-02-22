38a1ed294a001f2a961c5804de650b3c6a574159
Jadhavvarun <jadhavvarun67@gmail.com>
2024-02-22 23:25:47 +0530

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component'; 
+import { CardsComponent } from './cards/cards.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ NavbarComponent, RouterOutlet],
+  imports: [ NavbarComponent, CardsComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
